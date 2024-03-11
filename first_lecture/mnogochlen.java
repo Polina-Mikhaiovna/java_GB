2a27590cd95198419223620be0a353c2728d9dd5
Polina-Mikhaiovna <umrilovapolina@gmail.com>
2024-03-11 23:35:33 +0300

    Structure is almast ready, stated method of printing polinom with random coefficients

--- first_lecture/mnogochlen.java
+++ first_lecture/mnogochlen.java
@@ -2,28 +2,55 @@
 //список коэфициентов (от 0 до 100) многочлена заданной степени.
 
 import java.util.Scanner;
+import java.util.Random;
 
 public class Program {
 	
 	public static void main(String[] args) {
-		
 		int k = getK();
-
 		System.out.println(k);
-
+		int[] coefficients = new int[k];
+		coefficients = random(coefficients);
+		printIntArray(coefficients);
+		printPolinom(coefficients);
 	}
 	
 	public static int getK() {
-			Scanner iScanner = new Scanner(System.in);
-			System.out.printf("int k: ");
-			int k = 0;
-			if (iScanner.hasNextInt()) {
-				k = iScanner.nextInt();
-			}
-			iScanner.close();
-			return k;
+		Scanner iScanner = new Scanner(System.in);
+		System.out.printf("int k: ");
+		int k = 0;
+		if (iScanner.hasNextInt()) {
+			k = iScanner.nextInt();
 		}
-	public static int[] random {
-		
+		iScanner.close();
+		return k;
+	}
+	
+	public static int[] random(int[] randomAarray) {
+		Random r = new Random();
+		for (int i = 0; i < randomAarray.length; i++) {
+			randomAarray[i] = r.nextInt(100) + 1;
+		}
+
+		return randomAarray;
+	}
+
+	public static void printIntArray(int[] array){
+		for(int i = 0; i < array.length; i++)
+			System.out.printf("%d ", array[i]);
+	}
+
+	public static void printPolinom(int[] array){
+
+		for (int i = 0; i <= array.length; i++) {
+			if(i >= 1){
+				if(array[i] == 1){
+					System.out.printf("x**%d", i);
+					continue;
+				}
+				
+			}
+			else System.out.printf("%d = 0", array[i]);
+		}
 	}
 }
