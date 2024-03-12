2c5f97f8f9991100ce30cb6385374fe39ec07ebe
Polina-Mikhaiovna <umrilovapolina@gmail.com>
2024-03-12 23:14:56 +0300

    start of write/read file part

--- first_lecture/mnogochlen.java
+++ first_lecture/mnogochlen.java
@@ -1,21 +1,31 @@
-// Задана натуральная степень k. Сформировать случайным образом
-//список коэфициентов (от 0 до 100) многочлена заданной степени.
+// 1. Задана натуральная степень k. Сформировать случайным образом
+//    список коэфициентов (от 0 до 100) многочлена заданной степени.
+// 2. Даны 2 файла, в каждом находится запись многочлена. Нужно
+//    сформировать файл содержащий сумму многочленов.
 
 import java.util.Scanner;
 import java.util.Random;
+import java.util.Arrays;
+import java.io.FileWriter;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class Program {
 	
 	public static void main(String[] args) {
-		int k = getK();
-		System.out.println(k);
+		int k = getInt();
 		int[] coefficients = new int[k+1];
 		coefficients = random(coefficients);
-		printIntArray(coefficients);
-		printPolinom(coefficients);
+		createFile("file1.txt", createPolinom(coefficients));
+		createFile("file2.txt", createPolinom(coefficients));
+		String polinomFromFile1 = getStringFromFile("file1.txt");
+		String polinomFromFile2 = getStringFromFile("file2.txt");
+		parsingPolinom(polinomFromFile);
+
+
 	}
 	
-	public static int getK() {
+	public static int getInt() {
 		Scanner iScanner = new Scanner(System.in);
 		System.out.printf("int k: ");
 		int k = 0;
@@ -41,17 +51,44 @@ public class Program {
 			else System.out.printf("%d ", array[i]);
 	}
 
-	public static void printPolinom(int[] array){
+	public static String createPolinom(int[] array){
 		int l = array.length;
+		String res = "";
 		for (int i = 0; i < array.length; i++) {
 			l--;
 			if(i < array.length - 1){
 				if(array[i] != 1){
-					System.out.printf("%d*x**%d + ", array[i], l);
+					res = res + String.format("%d*x**%d + ", array[i], l);
 				}
-				else System.out.printf("x**%d + ", l);
+				else res = res + String.format("x**%d + ", l);
 			}
-			else System.out.printf("%d = 0\n", array[i]); // i == array.length - 1
+			else res = res + String.format("%d = 0\n", array[i]); // i == array.length - 1
+		}
+		return res;
+	}
+	public static void createFile(String name, String data) {
+		try(FileWriter fw = new FileWriter(name, false)) {
+			fw.write(data);
+			//fw.append("\n");
+			fw.flush();
+		} catch(IOException ex) {
+			System.out.println(ex.getMessage());
 		}
 	}
+	public static String getStringFromFile(String name) {
+		String res = "";
+		try(FileReader reader = new FileReader(name)) {
+           // читаем посимвольно
+            int c;
+            while((c = reader.read()) != -1){
+                res = res + ((char)c);
+            }
+        }
+        catch(IOException ex){
+            System.out.println(ex.getMessage());
+        }
+        return res;
+	}
 }
+
+
