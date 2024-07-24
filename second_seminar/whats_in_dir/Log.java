ac551bca5af1ef237b826a9719d4463bf5f91862
Polina-Mikhaiovna <umrilovapolina@gmail.com>
2024-07-24 14:28:34 +0300

    All works. Write files'names in directory to file. With logging

---
+++ second_seminar/whats_in_dir/Log.java
@@ -0,0 +1,35 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Arrays;
+import java.io.FileInputStream;
+import java.util.logging.LogManager;
+import java.util.logging.Logger;
+import java.util.logging.Level;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+public  class Log {
+	static {
+		try (FileInputStream in = new FileInputStream("../log.config")) {
+			LogManager.getLogManager().readConfiguration(in);
+		} catch (Exception e){
+			e.printStackTrace();
+		}
+	}
+
+	public static Logger log(String className) {
+		return Logger.getLogger(className);
+	}
+
+	public static void logException(IOException ex) {
+        String logFilePath = "exception_log.txt"; // путь к файлу лога в корне проекта
+        try (FileWriter logWriter = new FileWriter(logFilePath, true)) {
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            String timestamp = sdf.format(new Date());
+            logWriter.write(timestamp + " - " + ex.getMessage() + "\n");
+            logWriter.flush();
+        } catch (IOException logEx) {
+            System.out.println("Error writing to log file: " + logEx.getMessage());
+        }
+    }
+}
