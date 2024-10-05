3103e6c4e4a41e323403d0a00781a653601b544d
Polina-Mikhaiovna <umrilovapolina@gmail.com>
2024-10-05 20:24:51 +0300

    Generate CSV in file.csv

--- second_seminar/generateCSV/GenerateCSV.java
+++ second_seminar/generateCSV/GenerateCSV.java
@@ -15,6 +15,9 @@
 // Alice,25,Los Angeles
 // Bob,35,Chicago
 
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class GenerateCSV {
 
     public static void main(String[] args) {
@@ -24,19 +27,33 @@ public class GenerateCSV {
                            {"Alice", "25", "Los Angeles"},
                            {"Bob", "35", "Chicago"}
                            };
-        System.out.println(generateCSV(headers, data));
+        StringBuilder buildedCSV = new StringBuilder();
+        buildedCSV = generateCSV(headers, data);
+        String SBToStr = buildedCSV.toString();
+        String nameOfCsv = "DB.csv";
+        createFile(nameOfCsv, SBToStr);
+
     }
 
-    public static StringBuilder generateCSV(String[] head, String[][] data){
+    public static StringBuilder generateCSV(String[] head, String[][] colums){
         StringBuilder buildedCSV = new StringBuilder();
         String tempStr = new String();
         tempStr = String.join(", ", head);
         buildedCSV = buildedCSV.append(tempStr);
-        for(int i = 0; i < data.length; i++){
+        for(int i = 0; i < colums.length; i++){
             buildedCSV = buildedCSV.append("\n");
-            tempStr = String.join(", ", data[i]);
+            tempStr = String.join(", ", colums[i]);
                 buildedCSV = buildedCSV.append(tempStr);
         }
         return buildedCSV;
     }
+
+    public static void createFile(String name, String data) {
+		try(FileWriter fw = new FileWriter("second_seminar/generateCSV/" + name, false)) {
+			fw.write(data);
+			fw.flush();
+		} catch(IOException ex) {
+			System.out.println(ex.getMessage());
+		}
+	}
 }
