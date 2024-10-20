ef40a99273fb274933d297d92d2e8062f83d8f30
Polina-Mikhaiovna <umrilovapolina@gmail.com>
2024-10-20 22:11:20 +0300

    Can: create file (log.txt), find min and max values, write date, time, and values to log file

--- second_seminar/arraysOperationsLog/ArraysOperations.java
+++ second_seminar/arraysOperationsLog/ArraysOperations.java
@@ -19,7 +19,7 @@ public class ArraysOperations {
     public static void main(String[] args) {
         int[] arr = new int[] { 9, 4, 8, 3, 1 };
         
-        Path path = Path.of("second_seminar/generateCSV/log.txt");
+        Path path = Path.of("second_seminar/arraysOperationsLog/log.txt");
         if (!Files.exists(path));{
             File myFile = new File(path.toString());
             try {
@@ -42,12 +42,41 @@ public class ArraysOperations {
             if (arr[i] > max)
                 max = arr[i];
         }
-
+        logStep(min, max);
 
     }
 
-    // private static void logStep(int min, int max) {
+    private static void logStep(int min, int max) {
+        String dateTime = curentDateTime();
+        StringBuilder dateTimeMinMax = new StringBuilder();
+        dateTimeMinMax = dateTimeMinMax.append(dateTime + " " + min + " " + max);
 
-    // }
+        FileWriter fw = null;
+        try {
+			fw = new FileWriter("second_seminar/arraysOperationsLog/log.txt", true);
+            fw.write(dateTimeMinMax.toString());
+            fw.append("\n");
+			fw.flush();
+		} catch(IOException ex) {
+			System.out.println(ex.getMessage());
+		}
+        finally {
+            try {
+                if (fw != null) {
+                    fw.close();
+                }
+            } catch (IOException e) {
+                System.out.println("Failed to close the writer: " + e.getMessage());
+            }
+	    }
+    }
+    
+    public static String curentDateTime() {
+        StringBuilder dateTime = new StringBuilder();
+        Date dateNow = new Date();
+        SimpleDateFormat currentDateTime = new SimpleDateFormat("yyyy-MM-dd hh:mm");
+        dateTime = dateTime.append(currentDateTime.format(dateNow));
+        return dateTime.toString();
+    }
 
 }
