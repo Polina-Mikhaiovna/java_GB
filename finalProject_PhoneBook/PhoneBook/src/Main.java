3fff4ae2aac9bcd9b993a28d6591d02dcb870b8f
Polina-Mikhaiovna <umrilovapolina@gmail.com>
2025-01-01 22:17:07 +0300

    Added functions of deleting phone or name from phonebook, printing sorted Map of phonebook

--- finalProject_PhoneBook/PhoneBook/src/Main.java
+++ finalProject_PhoneBook/PhoneBook/src/Main.java
@@ -1,16 +1,31 @@
-import java.util.ArrayList;
-import java.util.List;
-
 public class Main {
         public static void main(String[] args) {
 
             PhoneBook phoneBook = new PhoneBook();
 
-            phoneBook.addPhone("John Doe", "123-456-7890");
-            phoneBook.addPhone("John Doe", "987-654-3210");
-            phoneBook.addPhone("Jane Smith", "555-123-4567");
+            phoneBook.addPhone("Ivan Ivanov", "+79169161616");
+            phoneBook.addPhone("Ivan Ivanov", "+79379373737");
+            phoneBook.addPhone("Peter Petrov", "+79229229292");
+            phoneBook.addPhone("Vasiliy Vasilyev", "+79199191919");
+            phoneBook.addPhone("Irina Irkova", "+79099099090");
+            phoneBook.addPhone("Irina Irkova", "+79019011010");
+            phoneBook.addPhone("Irina Irkova", "+79059059595");
+            phoneBook.addPhone("Darya Dashina", "+79129121212");
 
-            System.out.println(phoneBook);
+            System.out.println("Телефонная книга (HashMap)");
+            System.out.println(phoneBook + "\n");
+
+            System.out.println("Телефонная книга (отсортированная по убыванию количества телефонов)");
+            phoneBook.printMoreToLessPhones();
+
+            phoneBook.deletePhone("+79099099090");
+            System.out.println("Телефонная книга (после удаления номера +79099099090 методом deletePhone())");
+            phoneBook.printMoreToLessPhones();
+
+            phoneBook.deletePerson("Peter Petrov");
+            System.out.println("Телефонная книга (после удаления абонента Peter Petrov методом deletePerson())");
+            phoneBook.printMoreToLessPhones();
+
         }
 
 }
--- finalProject_PhoneBook/PhoneBook/src/PhoneBook.java
+++ finalProject_PhoneBook/PhoneBook/src/PhoneBook.java
@@ -1,4 +1,4 @@
-//Функционал
+// Функционал
 //        **put(K,V)** – добавить пару если или изменить значение,если ключ имеется.
 //        **putIfAbsent(K,V)** – произвести добавление если ключ не найден.
 //        **get(K)** - получение значения по указанному ключу.
@@ -7,6 +7,10 @@
 //        **containsKey(V)** – проверка наличия ключа.
 //        **keySet()** – возвращает множество ключей.
 //        **values()** – возвращает набор значений.
+//        **steam()** - API, потоки данных(многопоточность) выполнение разных действий параллельно, не мешая друг другу
+//      2 типа методов потока: конвейерные и терминальные
+//      Конв - применять методы к элементам потока, изменять, сортировать, элементы
+//      Терм - возвращают данные из состояния потока
 
 import java.util.*;
 
@@ -20,8 +24,35 @@ public class PhoneBook {
 
     public void addPhone(String name, String phone) {
         namePhone.putIfAbsent(name, new ArrayList<>());
-        // Добавляем номер телефона в список
         namePhone.get(name).add(phone);
+    }
+
+    /**
+     * Выводит отсортированную по убыванию количества элементов в списке значений мапу.
+     */
+    public void printMoreToLessPhones(){
+        namePhone.entrySet().stream()
+                .sorted((e1, e2) ->
+                Integer.compare(e2.getValue().size(),
+                e1.getValue().size()))
+                .forEach(entry -> System.out.println(entry.getKey() + ": " + entry.getValue()));
+        System.out.println("\n");
+    }
+
+    public void deletePhone(String phone){
+        for (Map.Entry<String, List<String>> pair : namePhone.entrySet()){
+            List<String> phones = pair.getValue();
+            if (phones.contains(phone)){
+                phones.remove(phone);
+                if (phones.isEmpty()){
+                    namePhone.remove(pair.getKey());
+                }
+            }
+        }
+
+    }
+    public void deletePerson(String name){
+            namePhone.remove(name);
     }
 
     @Override
@@ -40,8 +71,8 @@ public class PhoneBook {
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
-        for (Map.Entry<String, List<String>> entry : namePhone.entrySet()) {
-            sb.append(entry.getKey()).append(": ").append(entry.getValue()).append("\n");
+        for (Map.Entry<String, List<String>> pair : namePhone.entrySet()) {
+            sb.append(pair.getKey()).append(": ").append(pair.getValue()).append("\n");
         }
         return sb.toString();
     }
